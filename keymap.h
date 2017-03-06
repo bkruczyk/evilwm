e742f64a5093c85fa26dff82daa571d91ec1a52f
Bartłomiej Kruczyk <bartlomiej.kruczyk@gmail.com>
2017-03-06 16:24:07 +0100

    Change keybindings

--- keymap.h
+++ keymap.h
@@ -11,10 +11,10 @@
 #define KEY_RIGHT       XK_l
 #define KEY_DOWN        XK_j
 #define KEY_UP          XK_k
-#define KEY_LOWER       XK_Insert
+#define KEY_LOWER       XK_z
 #define KEY_ALTLOWER    XK_KP_Insert
 #define KEY_INFO        XK_i
-#define KEY_MAXVERT     XK_equal
+#define KEY_MAXVERT     XK_v
 #define KEY_MAX         XK_x
 #define KEY_DOCK_TOGGLE XK_d
 #ifdef VWM
